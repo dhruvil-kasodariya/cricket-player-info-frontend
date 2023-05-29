aa631c668a450333fa6eaf10a62f635c67db5944
iambunty45 <kasodariyadhruvil45@gmail.com>
2023-05-29 14:36:24 +0530

    teams preivie

--- src/componet/TeamsPreview/teams-preview.componet.jsx
+++ src/componet/TeamsPreview/teams-preview.componet.jsx
@@ -2,7 +2,7 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { setTeamDataSuccess } from "../../store/teams/teams.action";
 import { setPlayersDataNull } from "../../store/players/players.action";
-// TeamsPreviewCard
+
 import { TeamsPreviewContainer } from "./teams-preview.style";
 import { useEffect } from "react";
 import TeamCardView from "../CardView/team-card-view.componet";
